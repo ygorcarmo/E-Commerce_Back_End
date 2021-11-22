af319e84f47ea391e8047a7c97d961d6a6afdb6f
Ygor Oliveira do Carmo <ygor.carmo@gmail.com>
2021-11-22 17:39:37 +1100

    added delete method in product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -91,6 +91,25 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const delProd = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });  
+ 
+      // If ID does not exist in db, display error
+    if (!delProd) {
+     return res.status(404).json({ message: "Product Not Found" });
+    } else {
+     res.status(200).json({
+       message: "Product Deleted"
+     });
+     return 
+   };
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
